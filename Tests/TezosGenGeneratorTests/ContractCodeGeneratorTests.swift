83a48674e7a7a35885f3ce9e2520d854042f0595
Marek Fořt <marek.fort@ackee.cz>
2019-11-21 15:41:01 +0100

    Add test for combine code generation.

--- Tests/TezosGenGeneratorTests/ContractCodeGeneratorTests.swift
+++ Tests/TezosGenGeneratorTests/ContractCodeGeneratorTests.swift
@@ -40,6 +40,86 @@ final class ContractCodeGeneratorTests: TezosGenUnitTestCase {
         XCTAssertEqual(try fileHandler.readTextFile(fileHandler.currentPath.appending(component: "SharedContract.swift")), expectedContents)
     }
     
+    func test_shared_contract_is_generated_with_combine() throws {
+        // Given
+        let expectedContents = """
+        // Generated using TezosGen
+
+        import TezosSwift
+        import Combine
+
+        struct ContractMethodInvocation {
+            fileprivate let send: (_ from: Wallet, _ amount: TezToken, _ operationFees: OperationFees?, _ completion: @escaping RPCCompletion<String>) -> Cancelable?
+
+            init(send: @escaping (_ from: Wallet, _ amount: TezToken, _ operationFees: OperationFees?, _ completion: @escaping RPCCompletion<String>) -> Cancelable?) {
+                self.send = send
+            }
+
+            func send(from: Wallet, amount: TezToken, operationFees: OperationFees? = nil, completion: @escaping RPCCompletion<String>) -> Cancelable? {
+                self.send(from, amount, operationFees, completion)
+            }
+                
+            func sendPublisher(from: Wallet, amount: TezToken, operationFees: OperationFees? = nil) -> ContractPublisher {
+                ContractPublisher(send: { self.send(from, amount, operationFees, $0) })
+            }
+        }
+
+        typealias SendMethod = (@escaping RPCCompletion<String>) -> Cancelable?
+
+        class ContractSubscription<S: Subscriber>: Subscription where S.Input == String, S.Failure == TezosError {
+            private let send: SendMethod
+            private var hasValue = true
+            private var cancelable: Cancelable?
+            private var cancellable: Cancellable?
+            private var subscriber: S
+            
+            init(subscriber: S, send: @escaping SendMethod) {
+                self.subscriber = subscriber
+                self.send = send
+            }
+            
+            func request(_ demand: Subscribers.Demand) {
+                guard hasValue else { return }
+                hasValue = false
+                cancellable = Future { [weak self] promise in
+                    self?.cancelable = self?.send(promise)
+                }.sink(receiveCompletion: { [weak self] in
+                    self?.subscriber.receive(completion: $0)
+                }, receiveValue: { [weak self] in
+                    _ = self?.subscriber.receive($0)
+                })
+            }
+            
+            func cancel() {
+                cancelable?.cancel()
+                cancellable?.cancel()
+            }
+        }
+
+        struct ContractPublisher: Publisher {
+            typealias Output = String
+            typealias Failure = TezosError
+            
+            private let send: SendMethod
+            
+            init(send: @escaping SendMethod) {
+                self.send = send
+            }
+            
+            func receive<S>(subscriber: S) where S : Subscriber, Failure == S.Failure, Output == S.Input {
+                let subscription = ContractSubscription(subscriber: subscriber, send: send)
+                subscriber.receive(subscription: subscription)
+            }
+        }
+        """
+        
+        // When
+        try subject.generateSharedContract(path: fileHandler.currentPath, extensions: [.combine])
+        
+        // Then
+        XCTAssertEqual(try fileHandler.readTextFile(fileHandler.currentPath.appending(component: "SharedContract.swift")), expectedContents)
+    }
+    
     func test_contract_is_generated() throws {
         // Given
         let contract = Contract(storage: TezosElement(type: .set, args: [TezosElement(type: .nat)]), parameter: TezosElement(type: .set, args: [TezosElement(type: .nat)]))
