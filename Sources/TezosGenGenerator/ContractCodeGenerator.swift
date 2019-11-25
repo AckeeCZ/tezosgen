81957f53c2a59d1b57ad4dbf07cc5d744282f31f
Marek Fořt <marek.fort@ackee.cz>
2019-11-25 09:30:13 +0100

    Remove combine code that is implemented in TezosSwift.

--- Sources/TezosGenGenerator/ContractCodeGenerator.swift
+++ Sources/TezosGenGenerator/ContractCodeGenerator.swift
@@ -64,19 +64,20 @@ public final class ContractCodeGenerator: ContractCodeGenerating {
             case .combine:
                 return content + """
                     
+                    
                     func sendPublisher(from: Wallet, amount: TezToken, operationFees: OperationFees? = nil) -> ContractPublisher {
                         ContractPublisher(send: { self.send(from, amount, operationFees, $0) })
                     }
                 """
             }
         }
-        var contents = """
+        let contents = """
         // Generated using TezosGen
 
         import TezosSwift\(importExtensionContents)
 
         struct ContractMethodInvocation {
-            fileprivate let send: (_ from: Wallet, _ amount: TezToken, _ operationFees: OperationFees?, _ completion: @escaping RPCCompletion<String>) -> Cancelable?
+            private let send: (_ from: Wallet, _ amount: TezToken, _ operationFees: OperationFees?, _ completion: @escaping RPCCompletion<String>) -> Cancelable?
 
             init(send: @escaping (_ from: Wallet, _ amount: TezToken, _ operationFees: OperationFees?, _ completion: @escaping RPCCompletion<String>) -> Cancelable?) {
                 self.send = send
@@ -84,67 +85,10 @@ public final class ContractCodeGenerator: ContractCodeGenerating {
 
             func send(from: Wallet, amount: TezToken, operationFees: OperationFees? = nil, completion: @escaping RPCCompletion<String>) -> Cancelable? {
                 self.send(from, amount, operationFees, completion)
-            }
-            \(additionalFunctionContents)
+            }\(additionalFunctionContents)
         }
-        """
         
-        extensions.forEach {
-            switch $0 {
-            case .combine:
-                contents += """
-                
-                
-                typealias SendMethod = (@escaping RPCCompletion<String>) -> Cancelable?
-
-                class ContractSubscription<S: Subscriber>: Subscription where S.Input == String, S.Failure == TezosError {
-                    private let send: SendMethod
-                    private var hasValue = true
-                    private var cancelable: Cancelable?
-                    private var cancellable: Cancellable?
-                    private var subscriber: S
-                    
-                    init(subscriber: S, send: @escaping SendMethod) {
-                        self.subscriber = subscriber
-                        self.send = send
-                    }
-                    
-                    func request(_ demand: Subscribers.Demand) {
-                        guard hasValue else { return }
-                        hasValue = false
-                        cancellable = Future { [weak self] promise in
-                            self?.cancelable = self?.send(promise)
-                        }.sink(receiveCompletion: { [weak self] in
-                            self?.subscriber.receive(completion: $0)
-                        }, receiveValue: { [weak self] in
-                            _ = self?.subscriber.receive($0)
-                        })
-                    }
-                    
-                    func cancel() {
-                        cancelable?.cancel()
-                        cancellable?.cancel()
-                    }
-                }
-
-                struct ContractPublisher: Publisher {
-                    typealias Output = String
-                    typealias Failure = TezosError
-                    
-                    private let send: SendMethod
-                    
-                    init(send: @escaping SendMethod) {
-                        self.send = send
-                    }
-                    
-                    func receive<S>(subscriber: S) where S : Subscriber, Failure == S.Failure, Output == S.Input {
-                        let subscription = ContractSubscription(subscriber: subscriber, send: send)
-                        subscriber.receive(subscription: subscription)
-                    }
-                }
-                """
-            }
-        }
+        """
         
         let sharedContractPath = path.appending(component: "SharedContract.swift")
         try FileHandler.shared.write(contents, path: sharedContractPath, atomically: true)
@@ -232,6 +176,7 @@ public final class ContractCodeGenerator: ContractCodeGenerating {
         
         
             /// Call this method to obtain contract status data
+            @discardableResult
             func status(completion: @escaping RPCCompletion<
         """
         
